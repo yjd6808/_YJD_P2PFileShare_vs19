10c47b302bf5a4fd1a1bfb650891f2d0c4e6efed
wjdeh616 <wjdeh313@naver.com>
2020-02-16 02:16:33 +0900

    _

---
+++ FileTransferExampleProjects/06.a.UdpFileSender/NetworkMessage.cs
@@ -0,0 +1,15 @@
+﻿// Filename:  NetworkMessage.cs        
+// Author:    Benjamin N. Summerton <define-private-public>        
+// License:   Unlicense (http://unlicense.org/)      
+
+using System.Net;
+
+namespace UdpFileTransfer
+{
+    // This is a Simple datastructure that is used in packet queues
+    public class NetworkMessage
+    {
+        public IPEndPoint Sender { get; set; }
+        public Packet Packet { get; set; }
+    }
+}
